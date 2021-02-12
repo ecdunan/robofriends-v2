a9a494bc9a5c1a781e05564dc5f4ef4b3419ecbc
Emmanuel Dunan <dunan.emmanuel@gmail.com>
2021-02-12 12:34:37 +0800

    Implement fetching to API

--- pages/robots.tsx
+++ pages/robots.tsx
@@ -1,11 +1,26 @@
+import { GetStaticProps, InferGetStaticPropsType } from "next";
 import React, { useEffect, useState } from "react";
 import RobotList from "../components/robot-list";
 import SearchBar from "../components/searchbar/search-bar";
-import { IRobot, robots } from "../utils";
+import { IRobot } from "../utils";
 
-const Robots = () => {
-  const [ robotList, _setRobotList ] = useState<IRobot[]>(robots);
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+  return {
+    props: {
+      robots: data
+    },
+    revalidate: 1,
+  }
+}
+
+const Robots = ({ robots }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const [robotList, setRobotList] = useState<IRobot[]>([]);
   const [searchKeyword, setSearchKeyword] = useState("");
+
+  useEffect(() => { setRobotList(robots) }, [])
 
   const handleSearchChange = (keyword) => {
     setSearchKeyword(keyword)
@@ -18,11 +33,10 @@ const Robots = () => {
   return (
     <div className="bg-gradient-to-r from-green-800 to-blue-900 text-center min-h-screen py-10">
       <h1 className="text-green-300 font-sega text-4xl pb-5">Robofriends NextJS</h1>
-      <SearchBar searchKeyword={searchKeyword} onSearchChange={handleSearchChange}/>
+      <SearchBar searchKeyword={searchKeyword} onSearchChange={handleSearchChange} />
       <RobotList robots={filteredRobots} />
     </div>
   )
 }
-
 
 export default Robots;
